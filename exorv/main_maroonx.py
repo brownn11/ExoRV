28ddb71b1f7b982fa94745b5e4dd488441001e22
Nina Brown <ninamelissabrown@gmail.com>
2024-12-12 11:46:00 -0600

    simplified scp download, added a comparison feature, added optional offsets

--- exorv/main_maroonx.py
+++ exorv/main_maroonx.py
@@ -19,25 +19,27 @@ import load as l
 parser = argparse.ArgumentParser(description="Plot RVs")
 parser.add_argument('-TOI', '--TOI', type = int, required = False, default = 0, help = "Target TOI number.")
 parser.add_argument('-tn', '--target_name', type = str, required = False, default = "", help = "Target name (SIMBAD-recognizeable).")
-parser.add_argument('-f', '--folder_name', type = str, required = False, default = "", help = "SERVAL folder name.")
-parser.add_argument('-scp', '--scp_download', action = "store_true", help = "Download new files or use available.")
-parser.add_argument('-o', '--others', type = json.loads, help = "Additional or alternate planet parameters. Requires P (period [d]), t0 (epoch [BJD]), r (planet radius [earth rad]) OR m (planet mass [earth mass]), and m_s (stellar mass [sol mass]), with planet specified as '_pn'. \n ex: -o '{'P_p1':10, 't0_p1':5, 'r_p1':1, 'm_s':15}'")
+parser.add_argument('-scp_mx', '--scp_download_mx', type = str, required = False, default = "", help = "Input SERVAL folder name to indicate scp download.")
+parser.add_argument('-scp', '--scp_download', type = str, required = False, default = "", help = "Input full path + filename to data on remote server to indicate scp download.")
+parser.add_argument('-o', '--others', type = json.loads, help = """Additional or alternate planet parameters. Requires P (period [d]), t0 (epoch [BJD]), r (planet radius [earth rad]) OR m (planet mass [earth mass]), and m_s (stellar mass [sol mass]), with planet specified as '_pn'.\nex: -o '{"P_p1":10, "t0_p1":5, "r_p1":1, "m_s":15}' """)
 parser.add_argument('--points_only', action = "store_true", help = "Plot only RV points.")
 parser.add_argument('--curve_only', action = "store_true", help = "Plot only expected RV curve.")
-parser.add_argument('-p', '--local_path', type = str, required = False, default = '', help = 'Path to local RV data.')
+parser.add_argument('--compare', type = str, default = "", help = 'Enter another target name to compare.\nex: -tn "Gl 12_octd" --compare "Gl 12_novd"\Only works with points-only plots.')
+parser.add_argument('-p', '--local_path', type = str, required = False, default = '', help = 'Manually enter path to local RV data.')
+parser.add_argument('-n', '--file_name', type = str, required = False, default = '', help = 'Manually enter local file name of RV data.\nex: "Gl 15 A_cd_r.csv, Gl 15 A_cd_b.csv"')
+parser.add_argument('--offsets', action = "store_true", help = "Apply MAROON-X offsets")
 args = parser.parse_args()
 
-if args.scp_download:
-    if not args.folder_name:
-        print("SCP download requires a specified folder name. Please enter folder name:")
-        folder_name = input()
-    else:
-        folder_name = args.folder_name
-    l.loaddata(TOI = args.TOI, tn = args.target_name, servalfolder = folder_name)
+if args.scp_download_mx:
+    folder_name = args.scp_download_mx
+    l.loaddata_mx(TOI = args.TOI, tn = args.target_name, servalfolder = folder_name)
+elif args.scp_download:
+    path = args.scp_download
+    l.loaddata(path = path)
 
 if args.points_only:
-    p.points_only(tn =  args.target_name, TOI = args.TOI, path = args.local_path)
+    p.points_only(tn =  args.target_name, tn2 = args.compare, TOI = args.TOI, path = args.local_path, name = args.file_name, offsets = args.offsets)
 elif args.curve_only:
     p.curve_only(TOI = args.TOI, others = args.others)
 else:
-    p.RV_plotter(tn =  args.target_name, TOI = args.TOI, others = args.others, path = args.local_path)
+    p.RV_plotter(tn =  args.target_name, TOI = args.TOI, others = args.others, path = args.local_path, name = args.file_name, offsets = args.offsets)
