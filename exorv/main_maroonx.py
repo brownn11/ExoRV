1718fe247fe2a6766ac02d44e937b48e4988b4f4
Nina Brown <ninamelissabrown@gmail.com>
2025-02-09 18:54:14 -0600

    Removed --compare argument (added option to use multiple 'target names' instead). Added a --singleplot argument.

--- exorv/main_maroonx.py
+++ exorv/main_maroonx.py
@@ -18,16 +18,16 @@ import load as l
 # Parse arguments:
 parser = argparse.ArgumentParser(description="Plot RVs")
 parser.add_argument('-TOI', '--TOI', type = int, required = False, default = 0, help = "Target TOI number.")
-parser.add_argument('-tn', '--target_name', type = str, required = False, default = "", help = "Target name (SIMBAD-recognizeable).")
+parser.add_argument('-tn', '--target_name', type = str, required = False, default = "", help = "Target name as listed in filename (i.e. 'TOI-2142' for 'TOI-2142_r.csv').\nList multiple target names to overplot.")
 parser.add_argument('-scp_mx', '--scp_download_mx', type = str, required = False, default = "", help = "Input SERVAL folder name to indicate scp download.")
 parser.add_argument('-scp', '--scp_download', type = str, required = False, default = "", help = "Input full path + filename to data on remote server to indicate scp download.")
 parser.add_argument('-o', '--others', type = json.loads, help = """Additional or alternate planet parameters. Requires P (period [d]), t0 (epoch [BJD]), r (planet radius [earth rad]) OR m (planet mass [earth mass]), and m_s (stellar mass [sol mass]), with planet specified as '_pn'.\nex: -o '{"P_p1":10, "t0_p1":5, "r_p1":1, "m_s":15}' """)
 parser.add_argument('--points_only', action = "store_true", help = "Plot only RV points.")
 parser.add_argument('--curve_only', action = "store_true", help = "Plot only expected RV curve.")
-parser.add_argument('--compare', type = str, default = "", help = 'Enter another target name to compare.\nex: -tn "Gl 12_octd" --compare "Gl 12_novd"\Only works with points-only plots.')
 parser.add_argument('-p', '--local_path', type = str, required = False, default = '', help = 'Manually enter path to local RV data.')
 parser.add_argument('-n', '--file_name', type = str, required = False, default = '', help = 'Manually enter local file name of RV data.\nex: "Gl 15 A_cd_r.csv, Gl 15 A_cd_b.csv"')
 parser.add_argument('--offsets', action = "store_true", help = "Apply MAROON-X offsets")
+parser.add_argument('--singleplot', action = "store_true", help = "Plot red and blue channels together.")
 args = parser.parse_args()
 
 if args.scp_download_mx:
@@ -38,8 +38,8 @@ elif args.scp_download:
     l.loaddata(path = path)
 
 if args.points_only:
-    p.points_only(tn =  args.target_name, tn_adtl = args.compare, TOI = args.TOI, path = args.local_path, name = args.file_name, offsets = args.offsets)
+    p.points_only(tn =  args.target_name, TOI = args.TOI, path = args.local_path, name = args.file_name, offsets = args.offsets, singleplot = args.singleplot)
 elif args.curve_only:
     p.curve_only(TOI = args.TOI, others = args.others)
 else:
-    p.RV_plotter(tn =  args.target_name, tn_adtl = args.compare, TOI = args.TOI, others = args.others, path = args.local_path, name = args.file_name, offsets = args.offsets)
+    p.RV_plotter(tn =  args.target_name, TOI = args.TOI, others = args.others, path = args.local_path, name = args.file_name, offsets = args.offsets, singleplot = args.singleplot)
