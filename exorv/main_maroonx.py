ad5493e242b3285a43c136925dd37ca7e3d34474
Nina Brown <ninamelissabrown@gmail.com>
2025-10-05 12:55:50 -0700

    added a subfmt plot feature for transit obs. also added in known MX 2 m/s/day etalon drift.

--- exorv/main_maroonx.py
+++ exorv/main_maroonx.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 import numpy as np
-import sys
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
 from forecaster import forecaster3 as mr
-import glob
 import json
 plt.rcParams['lines.linewidth']=0.4
 plt.rcParams.update({'font.size': 8})
-import os
 import argparse
 
 import plot as p
@@ -22,12 +19,15 @@ parser.add_argument('-tn', '--target_name', type = str, required = False, defaul
 parser.add_argument('-scp_mx', '--scp_download_mx', type = str, required = False, default = "", help = "Input SERVAL folder name to indicate scp download.")
 parser.add_argument('-scp', '--scp_download', type = str, required = False, default = "", help = "Input full path + filename to data on remote server to indicate scp download.")
 parser.add_argument('-o', '--others', type = json.loads, help = """Additional or alternate planet parameters. Requires P (period [d]), t0 (epoch [BJD]), r (planet radius [earth rad]) OR m (planet mass [earth mass]), and m_s (stellar mass [sol mass]), with planet specified as '_pn'.\nex: -o '{"P_p1":10, "t0_p1":5, "r_p1":1, "m_s":15}' """)
-parser.add_argument('--points_only', action = "store_true", help = "Plot only RV points.")
-parser.add_argument('--curve_only', action = "store_true", help = "Plot only expected RV curve.")
+parser.add_argument('--pointsonly', action = "store_true", help = "Plot only RV points.")
+parser.add_argument('--curveonly', action = "store_true", help = "Plot only expected RV curve.")
 parser.add_argument('-p', '--local_path', type = str, required = False, default = '', help = 'Manually enter path to local RV data.')
 parser.add_argument('-n', '--file_name', type = str, required = False, default = '', help = 'Manually enter local file name of RV data.\nex: "Gl 15 A_cd_r.csv, Gl 15 A_cd_b.csv"')
 parser.add_argument('--offsets', action = "store_true", help = "Apply MAROON-X offsets")
 parser.add_argument('--singleplot', action = "store_true", help = "Plot red and blue channels together.")
+parser.add_argument('--MXdrift', action = "store_true", help = "Consider MX etalon drift of 2 m/s/day.")
+parser.add_argument('-subfmt', type = str, required = False, default = "date", help = "Astropy Time ISO subformat: date, date_hm, or date_hms")
+
 args = parser.parse_args()
 
 if args.scp_download_mx:
@@ -37,9 +37,9 @@ elif args.scp_download:
     path = args.scp_download
     l.loaddata(path = path)
 
-if args.points_only:
-    p.points_only(tn =  args.target_name, TOI = args.TOI, path = args.local_path, name = args.file_name, offsets = args.offsets, singleplot = args.singleplot)
-elif args.curve_only:
+if args.pointsonly:
+    p.points_only(tn =  args.target_name, TOI = args.TOI, path = args.local_path, name = args.file_name, offsets = args.offsets, singleplot = args.singleplot, subfmt = args.subfmt)
+elif args.curveonly:
     p.curve_only(TOI = args.TOI, others = args.others)
 else:
-    p.RV_plotter(tn =  args.target_name, TOI = args.TOI, others = args.others, path = args.local_path, name = args.file_name, offsets = args.offsets, singleplot = args.singleplot)
+    p.RV_plotter(tn =  args.target_name, TOI = args.TOI, others = args.others, path = args.local_path, name = args.file_name, offsets = args.offsets, singleplot = args.singleplot, MXdrift = args.MXdrift, subfmt = args.subfmt)
